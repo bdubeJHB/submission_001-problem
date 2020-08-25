ede1388a8c9b96c0a24829032dba933780551f7c
bdubeJHB <bdube@student.wethinkcode.co.za>
2020-08-25 18:00:38 +0200

    wtc-lms save

--- course.py
+++ course.py
@@ -1,10 +1,38 @@
+from random import randint
 
 
 def create_outline():
-    """
-    TODO: implement your code here
-    """
-    pass
+    topics = {"Introduction to Python", "Tools of the Trade", "How to make decisions", "How to repeat code", "How to structure data", "Functions", "Modules"}
+
+    print("Course Topics:")
+    for topic in topics:
+        print("* " + topic)
+
+    problems = dict()
+    for topic in topics:
+        problems.update({topic: ["Problem 1", "Problem 2", "Problem 3"]})
+
+    print("Problems:")
+    for problem in problems:
+        print("* " + problem + " : " + ", ".join(problems[problem]))
+    
+    status = ("STARTED", "COMPLETED", "GRADED")
+
+    students = [make_student("Moses", problems, status), make_student("Daniel", problems, status), make_student("Sarah", problems, status), make_student("Paul", problems, status), make_student("Ruth", problems, status), make_student("Isaac", problems, status), make_student("Jonah", problems, status)]
+
+    print("Student progress:")
+    num = 1
+    for student in students:
+        print(str(num) + ". " + )
+        num += 1
+
+
+def make_student(id, all_problems, statuses)
+    topics = list(all_problems.keys())
+    problems = list(all_problems.values())
+
+    student = (id, topics[random.randint(0, len(topics) - 1)], problems[random.randint(0, len(problems) - 1)], statuses[random.randint(0, len(statuses))])
+    return student
 
 
 if __name__ == "__main__":
